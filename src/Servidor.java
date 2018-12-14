bb4ca2c650d176ed8f25243bce46389a42567d5a
Francisco Azedo <franciscoazedo97@gmail.com>
2018-12-14 19:05:58 +0000

    Interface cliente com menus até ao utilizador implementados gestor de cliente só tem implementado o login, registo e lista de utilizadores

--- src/Servidor.java
+++ src/Servidor.java
@@ -1,16 +1,120 @@
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Servidor {
-    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost:3306/pd1819?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/pd1819?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     
     //username e password para aceder à base de dados
-    static final String USER = "root";
-    static final String PASS = "12345";
+    private static final String USER = "Francisco";
+    private static final String PASS = "12345";
     
-    static Connection conn = null;
-    static Statement stmt = null;
+    private static Connection conn = null;
+    private static Statement stmt = null;
+    
+    protected ServerSocket serverSocket;
+    
+    public static final int SERVICE_PORT = 5001;
+
+    public Servidor() {
+        try {
+            serverSocket = new ServerSocket(SERVICE_PORT);
+        } catch (IOException e) {
+            System.out.println("Ocorreu um erro a iniciar o servidor:\n\t"+e);
+        }
+    }
+   
+    //funcao que recebe ligacoes tcp e lança threads para atender cientes
+    public void receberClientes(){
+        while(true){
+            try {
+                Thread t = new AtendeCliente(serverSocket.accept(), this);
+                t.setDaemon(true);
+                t.start();
+            } catch (IOException e) {
+                System.out.println("Ocorreu um erro no acesso ao socket:\n\t"+e);
+            }
+        }
+    }
+    
+    //funcao verificar se nome utilizador ja existe
+    public int verificarUsername(String username){
+        
+        try {
+            String sql = "SELECT * FROM Utilizadores";
+            ResultSet rs = stmt.executeQuery(sql);
+            
+            while(rs.next()){
+                if (rs.getString("Username").equalsIgnoreCase(username))
+                    return -1;
+            }            
+        } catch (SQLException se) {
+            System.out.println(se);
+            return -2;
+        }
+        return 1;
+    }
+    
+    //funcao para registar utilizador
+    public int registarUser(String username, String password){
+        try {
+            String sql = "INSERT INTO Utilizadores (Username, Password) VALUES (\"" + username + "\", \"" + password + "\");";
+            stmt.executeUpdate(sql);          
+        } catch (SQLException se) {
+            System.out.println(se);
+            return -2;
+        }
+        return 1;
+    }
+    
+    //funcao para efetuar login
+    public int efetuarLogin(String username, String password){
+        try {
+            String sql = "SELECT * FROM Utilizadores WHERE Username = \"" + username + "\"";
+            ResultSet rs = stmt.executeQuery(sql);
+            if (!rs.next())
+                return -1;
+            else
+                if (!rs.getString("password").equalsIgnoreCase(password))
+                    return -3;
+                else{
+                    if (rs.getInt("Online") == 1)
+                        return -4;
+                    else{
+                        sql = "UPDATE Utilizadores SET Online = 1 WHERE Username = \"" + username + "\"";
+                        stmt.executeUpdate(sql);
+                    }
+                }            
+        } catch (SQLException se) {
+            System.out.println(se);
+            return -2;
+        }
+        return 1;
+    }
+
+    //funcao para retornar utilizadores online
+    public ArrayList<String> utilizadoresOnline(String username){
+        ArrayList<String> utilizadores = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM Utilizadores";
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()){
+                if (rs.getInt("Online") == 1 && !rs.getString("Username").equalsIgnoreCase(username))
+                    utilizadores.add(rs.getString("Username")); 
+            }
+        } catch (SQLException se) {
+            System.out.println(se);
+        }
+        return utilizadores;
+    }
     
     public static void main(String[] args) {
  
@@ -22,67 +126,88 @@ public class Servidor {
             System.out.println("Creating statement...");
             stmt = conn.createStatement();
             
-            System.out.println(verificarUsername("ze"));
-            System.out.println(registarUser("ze", "pass"));
-            System.out.println(efetuarLogin("ze", "pass"));
-            stmt.close(); 
-            conn.close();
+            new Servidor().receberClientes();
+            
         }catch(SQLException se){
             se.printStackTrace();
         }
         finally{
             try{
-                if(stmt!=null) stmt.close();
-            }catch(SQLException se2){}
+                if(stmt!=null) 
+                    stmt.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
             try{
-                if(conn!=null) conn.close();
-            }catch(SQLException se){ se.printStackTrace();}
+                if(conn!=null) 
+                    conn.close();
+            }catch(SQLException se){ 
+                se.printStackTrace();
+            }
         }
     }
+}
+
+class AtendeCliente extends Thread {
     
-    //funcao verificar se nome utilizador ja existe
-    public static boolean verificarUsername(String username){
+    private Socket cliente;
+    private static Servidor servidor;
+    private ObjectInputStream oin;
+    private ObjectOutputStream out;
+    
+    public AtendeCliente(Socket s, Servidor serv){
         try {
-            String sql = "SELECT * FROM Utilizadores";
-            ResultSet rs = stmt.executeQuery(sql);
-            
-            while(rs.next()){
-                if (rs.getString("username").equalsIgnoreCase(username))
-                    return false;
-            }            
-        } catch (SQLException se) {
-            System.out.println(se);
-            return false;
+            cliente = s;
+            servidor = serv;
+            oin = new ObjectInputStream(cliente.getInputStream());
+            out = new ObjectOutputStream(cliente.getOutputStream());
+        } catch (IOException e) {
+                System.out.println("Ocorreu um erro no acesso ao socket:\n\t"+e);
         }
-        return true;
+    } 
+    
+    public void run(){
+        while(!cliente.isClosed()){
+            try { 
+                Object pedido = oin.readObject();
+                if (pedido instanceof Pedido_Registo){                
+                    Pedido_Registo p = (Pedido_Registo) pedido;
+                    if(p.getTipo().equalsIgnoreCase("Registar")){
+                        switch(servidor.verificarUsername(p.getUsername())){
+                            case 1:
+                                p.resultado_Pedido(servidor.registarUser(p.getUsername(), p.getPassword()));
+                                break;
+                            case -1:
+                                p.resultado_Pedido(-1);
+                                break;
+                            case -2:
+                                p.resultado_Pedido(-2);
+                                break;
+                        }
+                    }
+                    else{
+                        p.resultado_Pedido(servidor.efetuarLogin(p.getUsername(), p.getPassword()));
+                    }
+                }
+                else if(pedido instanceof Pedido_Utilizadores){
+                    System.out.println("Pedido Utilizadores");
+                    Pedido_Utilizadores p = (Pedido_Utilizadores) pedido;
+                    p.setUtilizadores(servidor.utilizadoresOnline(p.getUsername()));
+                }
+
+                out.writeObject(pedido);
+                out.flush();
+
+            } catch (IOException e) {
+                try {
+                    System.out.println("Ocorreu um erro no acesso ao socket:\n\t"+e);
+                    cliente.close();
+                } catch (IOException ex) {
+                    System.out.println("Ocorreu um erro a fechar o socket:\n\t"+ex);
+                }
+            } catch (ClassNotFoundException e) {
+                Logger.getLogger(AtendeCliente.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
     }
-    
-    //funcao para registar utilizador
-    public static boolean registarUser(String username, String password){
-        try {
-            String sql = "INSERT INTO Utilizadores (Username, Password) VALUES (\"" + username + "\", \"" + password + "\");";
-            stmt.executeUpdate(sql);          
-        } catch (SQLException se) {
-            System.out.println(se);
-            return false;
-        }
-        return true;
-    }
-    
-    //funcao para efetuar login
-    public static int efetuarLogin(String username, String password){
-        try {
-            String sql = "SELECT * FROM Utilizadores WHERE Username = \"" + username + "\"";
-            ResultSet rs = stmt.executeQuery(sql);
-            if (!rs.next())
-                return -1;
-            else
-                if (!rs.getString("password").equalsIgnoreCase(password))
-                    return -2;            
-        } catch (SQLException se) {
-            System.out.println(se);
-            return -3;
-        }
-        return 1;
-    }    
 }
